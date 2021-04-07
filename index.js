4fb3f6863b1658a10f8c0538519d32878dd0fd8e
Will B <wberman27@gmail.com>
2021-04-07 19:13:46 -0400

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 5000;
+
+server.listen(port, ()=>{
+    console.log(`Success: Listening on Port ${port}`)
+    console.log(`Url: http://localhost:${port}`)
+})

caa2b2541f9657d1d39e0cb51609d9bcddcc9a45
Will B <wberman27@gmail.com>
2021-04-07 20:07:01 -0400

    added all middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -20,11 +20,21 @@ function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const name = req.body.name
+  if(!name){
+    res.status(400).json({ message: "missing required name field" })
+  }else{
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const post = req.body.text
+  if(!post){
+    res.status(400).json({ message: "missing required text field" })
+  }else{
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -21,28 +21,34 @@ router.get('/:id', mw.validateUserId, (req, res) => {
   res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', mw.validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+  .then(user =>{
+    res.status(201).json(user)
+  })
+  .catch(err =>{
+    res.status(500).json(err.message)
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', mw.validateUserId, mw.validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', mw.validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', mw.validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', mw.validateUserId, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
